da2d91a614608b3f6c393888be98c6eca7d9526c
Sergey Mozhnov <s.mozhnov.v@gmail.com>
2024-10-17 11:48:32 +0300

    4commit_fixed database queries

--- parcel.go
+++ parcel.go
@@ -16,7 +16,6 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
 	res, _ := s.db.Exec("INSERT INTO parcel (Client, Status, Address, created_at) VALUES (:Client, :Status, :Address, :created_at)",
-		//sql.Named("Number", p.Number),
 		sql.Named("Client", p.Client),
 		sql.Named("Status", p.Status),
 		sql.Named("Address", p.Address),
@@ -71,17 +70,18 @@ func (s ParcelStore) SetStatus(number int, status string) error {
 func (s ParcelStore) SetAddress(number int, address string) error {
 	// реализуйте обновление адреса в таблице parcel
 	// менять адрес можно только если значение статуса registered
-	_, err := s.db.Exec("UPDATE parcel SET Address = :Address WHERE Number = :Number, Status = :Status",
+	_, err := s.db.Exec("UPDATE parcel SET Address = :Address WHERE Number = :Number AND Status = :Status",
 		sql.Named("Address", address),
 		sql.Named("Number", number),
-		sql.Named("Status", ParcelStatusRegistered))
+		sql.Named("Status", ParcelStatusRegistered),
+	)
 	return err
 }
 
 func (s ParcelStore) Delete(number int) error {
 	// реализуйте удаление строки из таблицы parcel
 	// удалять строку можно только если значение статуса registered
-	_, err := s.db.Exec("DELETE FROM parcel WHERE Number = :Number, Status = :Status",
+	_, err := s.db.Exec("DELETE FROM parcel WHERE Number = :Number AND Status = :Status",
 		sql.Named("Number", number),
 		sql.Named("Status", ParcelStatusRegistered))
 	return err
